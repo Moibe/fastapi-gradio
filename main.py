5ef5dccfb868f119a18af14da125d9021f9c74f8
Moibe <moi.estrello@gmail.com>
2024-04-11 19:00:37 -0600

    1st Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+os.makedirs("static", exist_ok=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+videos = []
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "videos": videos})
+
+@app.post("/uploadvideo/")
+async def upload_video(video: UploadFile = File(...)):
+    new_video = process_video(video.filename)
+    videos.append(new_video)
+    return RedirectResponse(url='/', status_code=303)
